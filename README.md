217a29539de7046adc0b1425385cd74e7215b96c
Kevin <kevin.loiseleur@epitech.eu>
2017-09-14 15:18:49 +0200

    add Readme and server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Api Specifications
+
+Connection DB :
+SHELL : $ mongo ds135574.mlab.com:35574/ecvplanning -u <dbuser> -p <dbpassword>
+
+Url db :  mongodb://<dbuser>:<dbpassword>@ds135574.mlab.com:35574/ecvplanning
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app            = express();
 
 //# Port
 const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
+
 app.listen(port, () => {
     console.log('We are listening on ' + port);
 });
