d3cfa2527c8c63fb18bd395abeddb909f947b794
David Maarek <davidmaarek55@gmail.com>
2017-09-14 16:03:03 +0200

    Add hateos routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,11 +2,14 @@ module.exports = function (app, db) {
     app.get('/', (req, res) => {
         let hateoas = {
             "get_groups" : "/groups",
-        }
+            "get_courses_by_group" : "/courses/{group}",
+            "get_courses_by_group_and_day" : "/courses/{group}/{day}",
+            "get_course_by_id" : "/course/{id}"
+        };
         res.json(hateoas).status(200)
-    })
+    });
 
     app.get('/groups', (request, response) => {
 
     })
-}
+};
